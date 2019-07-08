61711faf1b5e1a892f22180d42654117ba822598
Jonathan Scholz <contact@jonathan-scholz.de>
2019-07-08 20:31:17 +0200

    feat: add iid and tests (#1)

--- go.mod
+++ go.mod
@@ -1,1 +1,6 @@
 module github.com/robojones/iid
+
+require (
+	github.com/pkg/errors v0.8.1
+	github.com/stretchr/testify v1.3.0
+)
---
+++ iid_test.go
@@ -0,0 +1,57 @@
+package iid
+
+import (
+	"encoding/binary"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestNew(t *testing.T) {
+	before := time.Now().Unix()
+	id := New()
+	after := time.Now().Unix()
+
+	idTime := int64(binary.BigEndian.Uint32(id))
+
+	assert.True(t, idTime >= before)
+	assert.True(t, idTime <= after)
+}
+
+func TestFromString(t *testing.T) {
+	ex := New()
+	s := enc.EncodeToString(ex)
+
+	id, err := FromString(s)
+
+	assert.Nil(t, err)
+	assert.Equal(t, ex, id)
+}
+
+func TestFromUint64(t *testing.T) {
+	ex := New()
+	i := binary.BigEndian.Uint64(ex)
+
+	id := FromUint64(i)
+
+	assert.Equal(t, ex, id)
+}
+
+func TestIid_String(t *testing.T) {
+	ex := New()
+
+	str := ex.String()
+
+	id, err := FromString(str)
+	assert.Nil(t, err)
+	assert.Equal(t, ex, id)
+}
+
+func TestIid_Uint64(t *testing.T) {
+	ex := New()
+
+	i := ex.Uint64()
+
+	id := FromUint64(i)
+	assert.Equal(t, ex, id)
+}
