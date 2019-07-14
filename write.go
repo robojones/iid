ef30a91bf3bc9a17b567613b01458d7f1de411b6
Jonathan Scholz <contact@jonathan-scholz.de>
2019-07-14 14:27:02 +0200

    docs: fix doc comment on writeTime method (#14)

--- write.go
+++ write.go
@@ -7,7 +7,7 @@ import (
 // dateOffset is the number of bits by which the date is offset to the right.
 const dateOffset = 1
 
-// Writes the provided t into b with an offset of 1 to the right.
+// writeTime writes the provided t into b with an offset of 1 to the right.
 func writeTime(b []byte, t uint32) {
 	if len(b) < 5 {
 		panic(errors.New("length of b is less than 5"))
